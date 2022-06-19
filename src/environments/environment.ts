0825e8e5bcd384779d0859ed866c77ab88c463f0
Davi Costa <davi.machado.costa@hotmail.com>
2022-06-19 19:50:54 -0300

    Adaptando environment para microserviços

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,9 +5,9 @@
 export const environment = {
   production: false,
   hmr: false,
-  api_admin: "http://localhost:3000/api",
-  api_avaliativo: "http://localhost:3001/api",
-  api_colab: "http://localhost:3002/api"
+  api_admin: "http://localhost:3000",
+  api_avaliativo: "http://localhost:3001",
+  api_colab: "http://localhost:3002"
 };
 
 /*
